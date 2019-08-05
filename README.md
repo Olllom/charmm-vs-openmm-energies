d4bf70e207f040defe413a7e76bfd099c367dbf1
Andreas Krämer <kraemer.research@gmail.com>
2019-08-05 17:50:40 -0400

    run script

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # charmm-vs-openmm-energies
+
+```
+NBFIX
+no long-range-correction              15559.71602
+dispersion correction (Allen-Tild.)   15497.11568
+long-range correction (Shirts et al.) 15490.09028
+DRUDE
+no long-range-correction              -1788.36644
+dispersion correction (Allen-Tild.)   -1832.17898
+long-range correction (Shirts et al.) -1835.06427
+```
--- run.py
+++ run.py
@@ -1,6 +1,32 @@
 import os
-charmm = "/charmm/c40b1_gnu/exec/gnu/charmm"
+#charmm = "/charmm/c40b1_gnu/exec/gnu/charmm"
+charmm = "c41b1"
+
+def run_test(directory, input_file="run.inp", parameters={}, output_file="run.out"):
+    olddir = os.getcwd()
+    os.chdir(directory)
+    env = "".join("{}={}".format(prm, parameters[prm]) for prm in parameters)
+    os.system("{} {} < {} > {}".format(charmm, env, input_file, output_file))
+    with open(output_file, 'r') as f:
+        for line in f:
+            if line.strip().startswith("ENER>"):
+                result = float(line.strip().split()[2])
+    os.chdir(olddir)
+    return result
 
 if __name__ == "__main__":
-    print("Hello")
+    print("NBFIX")
+    print( "no long-range-correction             ",
+            run_test("nbfix", "nbfixtest.inp", {"LRC":""}))
+    print( "dispersion correction (Allen-Tild.)  ",
+            run_test("nbfix", "nbfixtest.inp", {"LRC":"LRC"}))
+    print( "long-range correction (Shirts et al.)",
+            run_test("nbfix", "nbfixtest.inp", {"LRC":"LRC_MS"}))
+    print("DRUDE") 
+    print( "no long-range-correction             ",
+            run_test("drude", "drudetest.inp", {"LRC":""}))
+    print( "dispersion correction (Allen-Tild.)  ",
+            run_test("drude", "drudetest.inp", {"LRC":"LRC"}))
+    print( "long-range correction (Shirts et al.)",
+            run_test("drude", "drudetest.inp", {"LRC":"LRC_MS"}))
 
