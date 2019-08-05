b212dd4458e0e5943e0d42527e8a4a2789e9004c
Andreas Krämer <kraemer.research@gmail.com>
2019-08-05 18:44:00 -0400

    Assert energies in CI

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
+from __future__ import print_function
 import os
+
 charmm = "/charmm/c40b1_gnu/exec/gnu/charmm"
 #charmm = "c41b1"
 
-def run_test(directory, input_file="run.inp", parameters={}, output_file="run.out"):
+def run_test(directory, input_file, expected, parameters={}, output_file="run.out"):
     olddir = os.getcwd()
     os.chdir(directory)
     env = "".join("{}={}".format(prm, parameters[prm]) for prm in parameters)
@@ -12,21 +14,22 @@ def run_test(directory, input_file="run.inp", parameters={}, output_file="run.ou
             if line.strip().startswith("ENER>"):
                 result = float(line.strip().split()[2])
     os.chdir(olddir)
+    assert abs(result-expected) < 0.1
     return result
 
 if __name__ == "__main__":
     print("NBFIX")
     print( "no long-range-correction             ",
-            run_test("nbfix", "nbfixtest.inp", {"LRC":""}))
+            run_test("nbfix", "nbfixtest.inp", 15559.71602, {"LRC":""}))
     print( "dispersion correction (Allen-Tild.)  ",
-            run_test("nbfix", "nbfixtest.inp", {"LRC":"LRC"}))
+            run_test("nbfix", "nbfixtest.inp", 15497.11568, {"LRC":"LRC"}))
     print( "long-range correction (Shirts et al.)",
-            run_test("nbfix", "nbfixtest.inp", {"LRC":"LRC_MS"}))
+            run_test("nbfix", "nbfixtest.inp", 15490.09028, {"LRC":"LRC_MS"}))
     print("DRUDE") 
     print( "no long-range-correction             ",
-            run_test("drude", "drudetest.inp", {"LRC":""}))
+            run_test("drude", "drudetest.inp", -1788.36644, {"LRC":""}))
     print( "dispersion correction (Allen-Tild.)  ",
-            run_test("drude", "drudetest.inp", {"LRC":"LRC"}))
+            run_test("drude", "drudetest.inp", -1832.17898, {"LRC":"LRC"}))
     print( "long-range correction (Shirts et al.)",
-            run_test("drude", "drudetest.inp", {"LRC":"LRC_MS"}))
+            run_test("drude", "drudetest.inp", -1835.06427, {"LRC":"LRC_MS"}))
 
