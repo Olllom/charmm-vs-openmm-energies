17931537e95d942965db11187b2e074e7f0ede34
Andreas Krämer <kraemer.research@gmail.com>
2019-12-06 10:05:47 -0500

    check travis change

--- run.py
+++ run.py
@@ -1,13 +1,13 @@
 from __future__ import print_function
 import os
 
-charmm = "/charmm/c40b1_gnu/exec/gnu/charmm"
-#charmm = "c41b1"
+#charmm = "/charmm/c40b1_gnu/exec/gnu/charmm"
+charmm = "c41b1"
 
 def run_test(directory, input_file, expected, parameters={}, output_file="run.out"):
     olddir = os.getcwd()
     os.chdir(directory)
-    env = "".join("{}={}".format(prm, parameters[prm]) for prm in parameters)
+    env = " ".join("{}={}".format(prm, parameters[prm]) for prm in parameters)
     os.system("{} {} < {} 2>&1 | tee {}".format(charmm, env, input_file, output_file))
     with open(output_file, 'r') as f:
         for line in f:
@@ -33,9 +33,9 @@ if __name__ == "__main__":
     print( "long-range correction (Shirts et al.)",
             run_test("drude", "drudetest.inp", -1835.06427, {"LRC":"LRC_MS"}))
     print("VSWITCH")
-    print( "original parameters",
-            run_test("vswitch", "charmm_solv_energies.inp", -1835.06427, {"SOL":"m14", "SUB":"original"}))
-    print( "annihilated electrostatics",
-            run_test("vswitch", "charmm_solv_energies.inp", -1835.06427, {"SOL":"m14", "SUB":"uncharged"}))
-    print( "annihilated elec + vdw",
-            run_test("vswitch", "charmm_solv_energies.inp", -1835.06427, {"SOL":"m14", "SUB":"annihilated"}))
+    print( "original parameters                  ",
+            run_test("vswitch", "charmm_solv_energies.inp", -12627.7628, {"SOL":"m14", "SUB":"original"}))
+    print( "annihilated electrostatics           ",
+            run_test("vswitch", "charmm_solv_energies.inp", -12527.7425, {"SOL":"m14", "SUB":"uncharged"}))
+    print( "annihilated elec + vdw               ",
+            run_test("vswitch", "charmm_solv_energies.inp", -12527.2375, {"SOL":"m14", "SUB":"annihilated"}))
